d760f7f0a5f16d446d51bb4b5be8f38f7198f37b
Chandrakanth-Masuna <masunachandu@8223gmail.com>
2024-09-14 17:48:44 +0530

    Get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import { Games } from "./useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImagesUrl from "./getCroppedImagesUrl";
 
 interface Props {
   game: Games;
@@ -9,7 +10,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={getCroppedImagesUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
---
+++ src/components/getCroppedImagesUrl.tsx
@@ -0,0 +1,9 @@
+const getCroppedImagesUrl = (url: string) => {
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+};
+
+export default getCroppedImagesUrl;
+
+// https://api.rawg.io/media/crop/600/400/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg
