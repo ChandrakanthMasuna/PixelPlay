3ed9ddc5c54ff096373b8f88a4090fcd71c5ddaf
Chandrakanth-Masuna <masunachandu@8223gmail.com>
2024-09-22 17:54:31 +0530

    Refractor: Extract a query object

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,3 +1,4 @@
+import { GameQuery } from "../App";
 import useData from "./useData";
 import { Genre } from "./useGenres";
 export interface Platform {
@@ -16,12 +17,14 @@ export interface Games {
 }
 
 
-const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) =>
+const useGames = (
+  gameQuery: GameQuery
+) =>
   useData<Games>("/games", {
     params: {
-      genres: selectedGenre?.id,
-      platforms: selectedPlatform?.id
+      genres: gameQuery.genre?.id,
+      platforms: gameQuery.platform?.id
     }
   },
-    [selectedGenre?.id, selectedPlatform?.id])
+    [gameQuery])
 export default useGames;
